b41221acafe003d5262750b7eb6524566d1c8c85
brakhmanov <dumorph@gmail.com>
2023-12-07 00:28:04 +0100

    added new activity

--- app/src/main/java/com/example/simplemathapp/MainActivity.kt
+++ app/src/main/java/com/example/simplemathapp/MainActivity.kt
@@ -2,13 +2,16 @@ package com.example.simplemathapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var questionView : TextView
-    lateinit var answerView : EditText
+    lateinit var questionView: TextView
+    lateinit var answerView: EditText
+    var correctAnswer: Int = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,14 +22,39 @@ class MainActivity : AppCompatActivity() {
         questionView = findViewById(R.id.questionView)
         answerView = findViewById(R.id.answerView)
 
+        val button = findViewById<Button>(R.id.answerButton)
+
+        button.setOnClickListener {
+            handleAnswer()
+        }
+
         setNewQuestion()
 
     }
+
+    fun handleAnswer() {
+        val answeredCorrect = checkAnswer()
+
+        Log.d("!!!","Du svarade $answeredCorrect")
+
+        
+    }
+
+    fun checkAnswer() : Boolean {
+        // kolla vad användaren har svarat
+        val answerText = answerView.text.toString()
+        val answer = answerText.toIntOrNull()
+
+        // jämföra användarens svar med rätta svaret
+        return answer == correctAnswer
+    }
+
 
     fun setNewQuestion() {
         val firstNumber = (1..10).random()
         val secondNumber = (1..10).random()
 
+        correctAnswer = firstNumber + secondNumber
         questionView.text = "$firstNumber + $secondNumber ="
 
     }
